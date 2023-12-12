36f0820d63a58258a8c2fa73220cb947363613c6
mezhendosina <mensh.zheya@gmail.com>
2023-12-12 14:38:49 +0300

    [fix]: now app is buildable

--- compose-ui/build.gradle.kts
+++ compose-ui/build.gradle.kts
@@ -49,36 +49,25 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
-    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.4"
+//    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
+//    buildFeatures {
+//        compose = true
+//    }
 
     dependencies {
         implementation(project(":shared"))
 
-        val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
-        implementation(composeBom)
-        androidTestImplementation(composeBom)
+        implementation(compose.preview)
 
-        // Choose one of the following:
-        // Material Design 3
-        implementation("androidx.compose.material3:material3")
-        // or skip Material Design and build directly on top of foundational components
-        implementation("androidx.compose.foundation:foundation")
-        // or only import the main APIs for the underlying toolkit systems,
-        // such as input and measurement/layout
-        implementation("androidx.compose.ui:ui")
-
-        // Android Studio Preview support
-        implementation("androidx.compose.ui:ui-tooling-preview")
-        debugImplementation("androidx.compose.ui:ui-tooling")
+        implementation(compose.ui)
+        implementation(compose.foundation)
+        implementation(compose.material3)
 
         implementation(libs.decompose.decompose)
         implementation(libs.decompose.extensionsComposeJetbrains)
